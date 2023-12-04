36599e59627dd8bb4cb3c44c8aae84e5e0dc8759
IronWario <144746337+IronWario@users.noreply.github.com>
2023-12-03 18:36:42 -0600

    Add files via upload

---
+++ Project3/cgh0124Project2_header.h
@@ -0,0 +1,30 @@
+//#pragma once
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+
+const int numTests = 5;
+enum choices;// {
+    /*Add = 1,
+    Remove = 2,
+    Display = 3,
+    Search = 4,
+    Results = 5,
+    Quit = 6
+};*/
+struct Student; //{
+    /*string name;
+    int id;
+    int testsTaken;
+    int* scores;
+    double avg;*/
+//};
+
+int getNumber();
+void add_Student();
+void remove_Student(int removeID);
+void display();
+void search(int findID);
+int findMinimum(int* allScores, int arrSize);
+void exportResults();
---
+++ Project3/cgh0124Project2_main.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+#include "cgh0124Project2_header.h"
+using namespace std;
+
+enum choices {
+Add = 1,
+Remove = 2,
+Display = 3,
+Search = 4,
+Results = 5,
+Quit = 6
+};
+
+struct Student {
+    string name;
+    int id;
+    int testsTaken;
+    int* scores;
+    double avg;
+};
+
+int main()
+{
+    bool looping = true;
+    while (looping) {
+        cout << "1.Add\n2.Remove\n3.Display\n4.Search\n5.Results\n6.Quit\nEnter your choice: ";
+        int userSelect;
+        choices userSelected;
+        cin >> userSelect;
+        userSelected = static_cast<choices>(userSelect);
+        int idHold;
+
+        switch (userSelected) {
+        case 1:
+            add_Student();
+            break;
+        case 2:
+            cout << "Please enter the student's id: ";
+            cin >> idHold;
+            remove_Student(idHold);
+            break;
+        case 3:
+            display();
+            break;
+        case 4:
+            cout << "Please enter the student's id: ";
+            cin >> idHold;
+            search(idHold);
+            break;
+        case 5:
+            exportResults();
+            break;
+        case 6:
+            looping = false;
+            cout << "Bye!!!" << endl;
+            break;
+        default:
+            cout << "Incorrect choice. Please enter again." << endl;
+            break;
+        }
+    }
+    return 0;
+}
